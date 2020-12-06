83761cc2ef2c8cc88e974a93f5c6da717d27336f
waifonglee <waifonglee_@hotmail.com>
2020-12-06 14:18:16 +0800

    aoc d6 part 2

--- aoc/Six/six.py
+++ aoc/Six/six.py
@@ -7,13 +7,39 @@ def proc_1():
                 for c in line:
                     if c != '\n':
                         qns.add(c)
-                #print(qns)
             else:
                 num = num + len(qns)
-                #print(str(len(qns)))
-                #print(str(num))
                 qns.clear()
     return num + len(qns)
 
-num = proc_1()
+
+def proc_2():
+    num = 0
+    qns = {}
+    num_people = 0
+    with open('six.txt') as f:
+        for line in f:
+            if line != '\n':
+                num_people = num_people + 1
+                for c in line:
+                    if c != '\n':
+                        if qns.get(c) != None:
+                            n = qns.get(c)
+                            qns[c] = n + 1
+                        else:
+                            qns[c] = 1
+            else:
+                for v in qns.values():
+                    if v == num_people:
+                        num = num + 1
+                num_people = 0
+                qns.clear()
+
+    for v in qns.values():
+        if v == num_people:
+            num = num + 1
+    return num
+
+
+num = proc_2()
 print(str(num))
