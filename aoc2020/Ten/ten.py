3dbfade61db0c0e399e2c5c570e093f08eae2156
waifonglee <waifonglee_@hotmail.com>
2020-12-10 14:49:58 +0800

    aoc d10

---
+++ aoc2020/Ten/ten.py
@@ -0,0 +1,46 @@
+with open('ten.txt') as f:
+    adapters = sorted([int(line.strip()) for line in f])
+    adapters.insert(0, 0)
+    adapters.append(adapters[len(adapters)- 1] + 3)
+#print(adapters)
+
+def p1():
+    diff_one = 0
+    diff_three = 0
+    
+    for i in range(0, len(adapters) - 1):
+        if adapters[i + 1] - adapters[i] == 1:
+            diff_one += 1
+        elif adapters[i + 1] - adapters[i] == 3:
+            diff_three += 1
+    
+    return diff_one * (diff_three + 1)
+
+#print(str(p1()))
+memo = {}
+
+def num_of_ways(i):
+    global memo
+    if memo.get(i) != None:
+        return memo[i]
+    
+    if i == len(adapters) - 1:
+        return 1
+
+    num = 0
+    current = adapters[i]
+    for j in range(i + 1, len(adapters)):
+        if adapters[j] - current <= 3:
+            num += num_of_ways(j)
+        else:
+            break
+
+    memo[i] = num
+    return num
+    
+
+print(str(num_of_ways(0)))
+
+
+
+
